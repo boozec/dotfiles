e09cbff64ac2077c467a701df1133c87c7270123
Santo Cariotti <santo@dcariotti.me>
2025-01-26 18:09:55 +0100

    nvim: edit debug F-keys

--- private_dot_config/nvim/lua/dap_conf.lua
+++ private_dot_config/nvim/lua/dap_conf.lua
@@ -42,10 +42,10 @@ end
 -- Include everything after this
 
 
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<F12>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F5>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F6>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F7>', function() require('dap').step_out() end)
 vim.keymap.set('n', '<C-K>', function() require('dap').toggle_breakpoint() end)
 vim.api.nvim_create_user_command('DapRepeat',function() require('dap').run_last() end, {})
 vim.api.nvim_create_user_command('DapOpen',function() dapui.open() end, {})
